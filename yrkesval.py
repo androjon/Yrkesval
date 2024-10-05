26750d6c04862c49e9b9bdfd973a21d57069e2fd
androjon <jonfindahl@gmail.com>
2024-10-05 07:50:35 +0200

    Update yrkesval.py

--- yrkesval.py
+++ yrkesval.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import json
-import itertools
-import json
 
 from occupation_class import create_occupation_index
 from create_options import create_options
@@ -20,213 +18,166 @@ def import_occupationdata():
 
 @st.cache_data
 def import_options(input):
-    options_all, options_field, options_ssyk_level_4, options_occupations, options_titles = create_options(input)
-    return options_all, options_field, options_ssyk_level_4, options_occupations, options_titles
+    options_field, options_ssyk_level_4, options_occupations, options_titles = create_options(input)
+    return options_field, options_ssyk_level_4, options_occupations, options_titles
 
-@st.cache_data
-def show_info_selected(fields, groups, occupation, title, description, taxonomy):
-    #with st.sidebar:
-    PIPE = "│"
-    ELBOW = "└──"
-    SHORT_ELBOW = "└─"
-    LONG_ELBOW = "    └──"
-    TEE = "├──"
-    PIPE_PREFIX = "│   "
-    SPACE_PREFIX = "&nbsp;&nbsp;&nbsp;&nbsp;"
-    strings = []
+def show_info_selected_sidebar(fields, groups, occupation, description, taxonomy):
+    with st.sidebar:
+        SHORT_ELBOW = "└─"
+        SPACE_PREFIX = "&nbsp;&nbsp;&nbsp;&nbsp;"
+        strings = []
+        if fields:
+            strings.append(fields)
+        if groups:
+            group_str = SHORT_ELBOW + groups
+            strings.append(group_str)
+        if occupation:
+            occupations_str = SPACE_PREFIX + SHORT_ELBOW + occupation
+            strings.append(occupations_str)
+        string = "<br />".join(strings)
+        tree = f"<p style='font-size:10px;'>{string}</p>"
+        st.markdown(tree, unsafe_allow_html=True)
+        definition = f"<p style='font-size:10px;'>{description}</p>"
+        st.markdown(definition, unsafe_allow_html=True)
+        if taxonomy:
+            taxonomy_text = taxonomy[0:5]
+            taxonomy_text_string = "<br />".join(taxonomy_text)
+            taxonomy_text_string = f"Efterfrågade kompetenser:<br />{taxonomy_text_string}"
+            taxonomy_text = f"<p style='font-size:10px;'>{taxonomy_text_string}</p>"
+            st.markdown(taxonomy_text, unsafe_allow_html=True)
+
+def show_info_selected(description):
+    definition = f"<p style='font-size:10px;'>{description}</p>"
+    st.markdown(definition, unsafe_allow_html=True)
+
+def show_initial_information():
+    st.logo("af-logotyp-rgb-540px.jpg")
+    st.title("Yrkesväljare")
+
+def change_state_chosen_background():
+    st.session_state.chosen_background = False
+
+def initiate_session_state():
+    if "chosen_background" not in st.session_state:
+        st.session_state.chosen_background = False
+        st.session_state.stored_backgrounds = []
+        st.session_state.words_of_experience = []
+        st.session_state.words_of_interest = []
+        st.session_state.similar_occupations = []
+
+    if len(st.session_state.stored_backgrounds) > 2:
+        st.button("Testa om annons- och utbildningsdata kan hjälpa dig att upptäcka dina dolda kompetenser") #on_click = ?
+
+    if st.session_state.chosen_background == True:
+        st.button("Lägga till fler yrkes- eller utbildningsbakgrunder", on_click = change_state_chosen_background)
+
+def create_valid_options(input, fields, groups, occupations, titles):
+    options_field, options_ssyk_level_4, options_occupations, options_titles = import_options(input)
+    output = {}
     if fields:
-        strings.append(fields)
+        output = output | dict(sorted(options_field.items(), key = lambda item: item[0]))
     if groups:
-        group_str = SHORT_ELBOW + groups
-        strings.append(group_str)
-    if occupation:
-        occupations_str = SPACE_PREFIX + SHORT_ELBOW + occupation
-        strings.append(occupations_str)
-    if title:
-        title_str = SPACE_PREFIX + SPACE_PREFIX + SHORT_ELBOW + title
-        strings.append(title_str)
-    string = "<br />".join(strings)
-    tree = f"<p style='font-size:12px;'>{string}</p>"
-    st.markdown(tree, unsafe_allow_html=True)
+        output = output | dict(sorted(options_ssyk_level_4.items(), key = lambda item: item[0]))
+    if occupations:
+        output = output | dict(sorted(options_occupations.items(), key = lambda item: item[0]))
+    if titles:
+        output = output | dict(sorted(options_titles.items(), key = lambda item: item[0]))
+    return output
 
-    text = f"<p style='font-size:12px;'>{description}</p>"
-    st.markdown(text, unsafe_allow_html=True)
+def post_selected_ssyk_level_4(id_ssyk_level_4):
+    ssyk_level_4_definition = st.session_state.definitions.get(id_ssyk_level_4)
+    show_info_selected(ssyk_level_4_definition)
+    occupation_name_options = {}
+    for o in st.session_state.occupationdata[id_ssyk_level_4].related_occupation:
+        occupation_name_options[st.session_state.occupationdata[o].showname] = st.session_state.occupationdata[o].id
+    choose_occupation_name(occupation_name_options)
 
-    if taxonomy:
-        taxonomy_text = taxonomy[0:5]
-        taxonomy_text_string = "<br />".join(taxonomy_text)
-        taxonomy_text = f"<p style='font-size:12px;'>Efterfrågade kompetenser:<br />{taxonomy_text_string}</p>"
-        st.markdown(taxonomy_text, unsafe_allow_html=True)
-
-st.logo("af-logotyp-rgb-540px.jpg")
-
-st.title("Yrkesväljare")
-
-text1 = "Vill du starta om tryck cmd + r"
-st.markdown(f"<p style='font-size:12px;'>{text1}</p>", unsafe_allow_html=True)
-
-data_occupations = import_occupationdata()
-
-options_all, options_field, options_ssyk_level_4, options_occupations, options_titles = import_options(data_occupations)
-
-descriptions = import_data("id_definitions.json")
-taxonomy_connections = import_data("taxonomy.json")
-
-st.write("Välj nedan om du vill exkludera något från den översta rullistan")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    exclude_field = st.toggle("yrkesområden", value = False)
-    exclude_groups = st.toggle("yrkesgrupper", value = False)
-
-with col2:
-    exclude_occupations = st.toggle("yrkesbenämningar", value = False)
-    exclude_titles = st.toggle("jobbtitlar", value = False)
-
-valid_options_dict = {}
-type_sorted_valid_options_list = []
-
-if exclude_field == False:
-    valid_options_dict = valid_options_dict | options_field
-    sorted_field = sorted(list(options_field.keys()))
-    type_sorted_valid_options_list.extend(sorted_field)
-if exclude_groups == False:
-    valid_options_dict = valid_options_dict | options_ssyk_level_4
-    sorted_groups = sorted(list(options_ssyk_level_4.keys()))
-    type_sorted_valid_options_list.extend(sorted_groups)
-if exclude_occupations == False:
-    valid_options_dict = valid_options_dict | options_occupations
-    sorted_occupations = sorted(list(options_occupations.keys()))
-    type_sorted_valid_options_list.extend(sorted_occupations)
-if exclude_titles == False:
-    valid_options_dict = valid_options_dict | options_titles
-    sorted_titles = sorted(list(options_titles.keys()))
-    type_sorted_valid_options_list.extend(sorted_titles)
-
-valid_options_list = sorted(list(valid_options_dict.keys()))
-
-selected_option = st.selectbox(
-    "Välj ett yrke/område som du tidigare har arbetat som/inom",
-    (type_sorted_valid_options_list), placeholder = "", index = None)
-
-if selected_option:
-    id_selected_option = valid_options_dict.get(selected_option)
-    type_selected_option = data_occupations[id_selected_option].type
-    related_field_names = []
-    related_group_names = []
-    related_occupation_names = []
-    for f in data_occupations[id_selected_option].related_occupation_fields:
-        related_field_names.append(data_occupations[f].id)
-    for g in data_occupations[id_selected_option].related_occupation_groups:
-        related_group_names.append(data_occupations[g].id)
-    for o in data_occupations[id_selected_option].related_occupation:
-        related_occupation_names.append(data_occupations[o].id)
-
-    if type_selected_option == "yrkesområde":
-        description = descriptions.get(id_selected_option)
-        show_info_selected(data_occupations[id_selected_option].showname, None, None, None, description, None)
-        valid_group_names_dict = {}
-        for g in related_group_names:
-            valid_group_names_dict[data_occupations[g].name] = g
-        valid_group_names_list = sorted(list(valid_group_names_dict.keys()))
-        selected_option_below_field = st.selectbox(
-            "Välj en yrkesgrupp",
-            (valid_group_names_list), placeholder = "", index = None)
-        
-        if selected_option_below_field:
-            id_selected_group = valid_group_names_dict.get(selected_option_below_field)
-            description = descriptions.get(id_selected_group)
-            show_info_selected(data_occupations[id_selected_option].showname, data_occupations[id_selected_group].showname, None, None, description, None)
-            related_occupation_names = data_occupations[id_selected_group].related_occupation
-            valid_occupation_names_dict = {}
-            for g in related_occupation_names:
-                valid_occupation_names_dict[data_occupations[g].name] = g
-            valid_occupation_names_list = sorted(list(valid_occupation_names_dict.keys()))
-            selected_option_below_group = st.selectbox(
-                "Välj en yrkesbenämning",
-                (valid_occupation_names_list), placeholder = "", index = None)
-            
-            if selected_option_below_group:
-                id_selected_option_below_group = valid_occupation_names_dict.get(selected_option_below_group)
-                description = descriptions.get(id_selected_option_below_group)
-                related_taxonomy = taxonomy_connections.get(id_selected_option_below_group)
-                show_info_selected(data_occupations[id_selected_option].showname, data_occupations[id_selected_group].showname, data_occupations[id_selected_option_below_group].showname, None, description, related_taxonomy)
+def post_selected_occupation(id_occupation):
+    related_fields = st.session_state.occupationdata[id_occupation].related_occupation_fields
+    related_fields_names = []
+    for f in related_fields:
+        related_fields_names.append(st.session_state.occupationdata[f].showname)
+    related_fields_str = "<br />".join(related_fields_names)
+    related_groups = st.session_state.occupationdata[id_occupation].related_occupation_groups
+    related_groups_names = []
+    for g in related_groups:
+        related_groups_names.append(st.session_state.occupationdata[g].showname)
+    related_groups_str = "<br />".join(related_groups_names)
+    definition_occupation = st.session_state.definitions.get(id_occupation)
+    related_taxonomy = st.session_state.taxonomy.get(id_occupation)
+    show_info_selected_sidebar(related_fields_str, related_groups_str, st.session_state.occupationdata[id_occupation].showname, definition_occupation, related_taxonomy)
     
-    elif type_selected_option == "yrkesgrupp":
-        name = data_occupations[id_selected_option].showname
-        related_fields = data_occupations[id_selected_option].related_occupation_fields
-        related_fields_names = []
-        for f in related_fields:
-            related_fields_names.append(data_occupations[f].showname)
-        related_fields_str = "<br />".join(related_fields_names)
-        description = descriptions.get(id_selected_option)
-        show_info_selected(related_fields_str, name, None, None, description, None)
 
-        valid_occupation_names_dict = {}
-        for g in related_occupation_names:
-            valid_occupation_names_dict[data_occupations[g].name] = g
-        valid_occupation_names_list = sorted(list(valid_occupation_names_dict.keys()))
-        selected_option_below_group = st.selectbox(
-            "Välj en yrkesbenämning",
-            (valid_occupation_names_list), placeholder = "", index = None)
-        
-        if selected_option_below_group:
-            id = valid_occupation_names_dict.get(selected_option_below_group)
-            name = data_occupations[id].showname
-            related_fields = data_occupations[id].related_occupation_fields
-            related_fields_names = []
-            for f in related_fields:
-                related_fields_names.append(data_occupations[f].showname)
-            related_fields_str = "<br />".join(related_fields_names)
-            related_groups = data_occupations[id].related_occupation_groups
-            related_groups_names = []
-            for g in related_groups:
-                related_groups_names.append(data_occupations[g].showname)
-            related_groups_str = "<br />".join(related_groups_names)
-            description = descriptions.get(id)
-            related_taxonomy = taxonomy_connections.get(id)
-            show_info_selected(related_fields_str, related_groups_str, name, None, description, related_taxonomy)
+def choose_ssyk_level_4(field_id):
+    ssyk_level_4_options = {}
+    for g in st.session_state.occupationdata[field_id].related_occupation_groups:
+        ssyk_level_4_options[st.session_state.occupationdata[g].showname] = st.session_state.occupationdata[g].id
+    valid_ssyk_level_4 = list(ssyk_level_4_options.keys())
+    selected_ssyk_level_4 = st.selectbox(
+        "Välj en yrkesgrupp",
+        (valid_ssyk_level_4), placeholder = "", index = None)
+    if selected_ssyk_level_4:
+        id_selected_ssyk_level_4 = ssyk_level_4_options.get(selected_ssyk_level_4)
+        ssyk_level_4_definition = st.session_state.definitions.get(id_selected_ssyk_level_4)
+        show_info_selected(ssyk_level_4_definition)
+        post_selected_ssyk_level_4(id_selected_ssyk_level_4)
 
-    elif type_selected_option == "jobbtitel":
-        valid_occupation_names_dict = {}
-        for g in related_occupation_names:
-            valid_occupation_names_dict[data_occupations[g].name] = g
-        valid_occupation_names_list = sorted(list(valid_occupation_names_dict.keys()))
-        selected_option_above_title = st.selectbox(
-            "Välj en yrkesbenämning",
-            (valid_occupation_names_list), placeholder = "", index = None)
+def choose_occupation_name(dict_valid_occupations):
+    valid_occupations = list(dict_valid_occupations.keys())
+    selected_occupation_name = st.selectbox(
+        "Välj en yrkesbenämning",
+        (valid_occupations), placeholder = "", index = None)
+    if selected_occupation_name:
+        id_selected_occupation = dict_valid_occupations.get(selected_occupation_name)
+        post_selected_occupation(id_selected_occupation)
         
-        if selected_option_above_title:
-            selected_option_above_title_id = valid_occupation_names_dict.get(selected_option_above_title)
-            name = data_occupations[selected_option_above_title_id].showname
-            related_fields = data_occupations[selected_option_above_title_id].related_occupation_fields
-            related_fields_names = []
-            for f in related_fields:
-                related_fields_names.append(data_occupations[f].showname)
-            related_fields_str = "<br />".join(related_fields_names)
-            related_groups = data_occupations[selected_option_above_title_id].related_occupation_groups
-            related_groups_names = []
-            for g in related_groups:
-                related_groups_names.append(data_occupations[g].showname)
-            related_groups_str = "<br />".join(related_groups_names)
-            description = descriptions.get(selected_option_above_title_id)  
-            related_taxonomy = taxonomy_connections.get(selected_option_above_title_id)    
-            show_info_selected(related_fields_str, related_groups_str, data_occupations[selected_option_above_title_id].showname, data_occupations[id_selected_option].showname, description, related_taxonomy)
-        
-    elif type_selected_option == "yrkesbenämning":
-        id = valid_options_dict.get(selected_option)
-        name = data_occupations[id].showname
-        related_fields = data_occupations[id].related_occupation_fields
-        related_fields_names = []
-        for f in related_fields:
-            related_fields_names.append(data_occupations[f].showname)
-        related_fields_str = "<br />".join(related_fields_names)
-        related_groups = data_occupations[id].related_occupation_groups
-        related_groups_names = []
-        for g in related_groups:
-            related_groups_names.append(data_occupations[g].showname)
-        related_groups_str = "<br />".join(related_groups_names)
-        description = descriptions.get(id)
-        related_taxonomy = taxonomy_connections.get(id)
-        show_info_selected(related_fields_str, related_groups_str, name, None, description, related_taxonomy)   
+def choose_occupational_background():
+    st.session_state.occupationdata = import_occupationdata()
+    st.session_state.definitions = import_data("id_definitions.json")
+    st.session_state.taxonomy = import_data("id_taxonomy.json")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        exclude_fields = st.toggle("inkludera yrkesområden", value = True)
+        exclude_groups = st.toggle("inkludera yrkesgrupper", value = False)
+
+    with col2:
+        exclude_occupations = st.toggle("inkludera yrkesbenämningar", value = False)
+        exclude_titles = st.toggle("inkludera jobbtitlar", value = False)
+
+    valid_options_dict = create_valid_options(st.session_state.occupationdata, exclude_fields, exclude_groups, exclude_occupations, exclude_titles)
+    valid_options_list = list(valid_options_dict.keys())
+
+    selected_option_occupation_field = st.selectbox(
+    "Välj ett yrke/område som du tidigare har arbetat som/inom",
+    (valid_options_list), placeholder = "", index = None)
+
+    if selected_option_occupation_field:
+        id_selected_option_occupation_field = valid_options_dict.get(selected_option_occupation_field)
+        type_selected_option_occupation_field = st.session_state.occupationdata[id_selected_option_occupation_field].type
+
+        if type_selected_option_occupation_field == "yrkesområde":
+            field_definition = st.session_state.definitions.get(id_selected_option_occupation_field)
+            show_info_selected(field_definition)
+            choose_ssyk_level_4(id_selected_option_occupation_field)
+
+        if type_selected_option_occupation_field == "yrkesgrupp":
+            post_selected_ssyk_level_4(id_selected_option_occupation_field)
+
+        if type_selected_option_occupation_field == "jobbtitel":
+            occupation_name_options = {}
+            for o in st.session_state.occupationdata[id_selected_option_occupation_field].related_occupation:
+                occupation_name_options[st.session_state.occupationdata[o].showname] = st.session_state.occupationdata[o].id            
+            choose_occupation_name(occupation_name_options)
+
+        if type_selected_option_occupation_field == "yrkesbenämning":
+            post_selected_occupation(id_selected_option_occupation_field)
+
+def main ():
+    show_initial_information()
+    initiate_session_state()
+    choose_occupational_background()
+
+if __name__ == '__main__':
+    main ()
