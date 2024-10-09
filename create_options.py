5fb4e9f46ba7092208f55aa2dc72d50ea78ebcbe
androjon <jonfindahl@gmail.com>
2024-10-09 08:11:03 +0200

    Update create_options.py

--- create_options.py
+++ create_options.py
@@ -1,11 +1,9 @@
 def create_options(input):
-    options_all = {}
     options_field = {}
     options_ssyk_level_4 = {}
     options_occupations = {}
     options_titles = {}
     for v in input.values():
-        options_all[v.showname] = v.id
         if v.type == "yrkesområde":
             options_field[v.showname] = v.id
         elif v.type == "yrkesgrupp":
@@ -14,4 +12,4 @@ def create_options(input):
             options_occupations[v.showname] = v.id
         elif v.type == "jobbtitel":
             options_titles[v.showname] = v.id
-    return options_all, options_field, options_ssyk_level_4, options_occupations, options_titles
+    return options_field, options_ssyk_level_4, options_occupations, options_titles
